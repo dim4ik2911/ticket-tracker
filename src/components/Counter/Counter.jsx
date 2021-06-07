e5d343d78d1630d38ac8e249472463ebf7c312e2
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-06-07 16:30:38 +0100

    hola

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styles from "./Counter.module.scss";
+import { useState } from "react";
 
 const Counter = () => {
-  let count = 0;
+  const [count, setCount] = useState(0);
+  const countMinusOne = () => {
+    return setCount(count - 1);
+  };
+  const countPlusOne = () => {
+    return setCount(count + 1);
+  };
+  {
+    /* <button onClick={() => setCount(count + 1)}></button> */
+  }
   return (
     <div className={styles.counter}>
       <p> Counter</p>
       <p> {count}</p>
       <div className={styles.buttons}>
-        <span>-</span>
-        <span>+</span>
+        <span onClick={countMinusOne}>-</span>
+        <span onClick={countPlusOne}>+</span>
       </div>
     </div>
   );
